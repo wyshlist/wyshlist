f977429f5568ec386ce24dd80f24ee0358e97262
Daniel Rodriguez <daniel.rod93@gmail.com>
2023-03-11 11:58:54 +0100

    FeaT: added some links in oganization create

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,5 @@
 class Organization < ApplicationRecord
     has_many :users, dependent: :destroy
     has_many :wishlists, through: :users
+    validates :name, presence: true, uniqueness: true
 end
