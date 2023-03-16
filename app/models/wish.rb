44c62c784a1248a5f85f7a90a1c6eea199273cd2
Daniel Rodriguez <daniel.rod93@gmail.com>
2023-03-16 15:07:03 +0100

    Fix: in process of making the tubro streams work

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -7,4 +7,5 @@ class Wish < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   enum status: { "Backlog": 0, "In process": 1, "In review": 2, "Beta": 3, "launched": 4 }
+  after_create_commit { broadcast_append_to "wishes" }
 end
