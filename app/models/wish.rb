4e9cf5b9fea73725d90489d2d57b4e3542b67ca1
Daniel Rodriguez <daniel.rod93@gmail.com>
2023-03-18 15:53:57 +0100

    FeaT: addedtags pills and edit of wish idea

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -6,6 +6,18 @@ class Wish < ApplicationRecord
   has_many :users
   belongs_to :user
   has_many :comments, dependent: :destroy
-  enum status: { "Backlog": 0, "In process": 1, "In review": 2, "Beta": 3, "launched": 4 }
+  enum stage: { "Backlog": 0, "In process": 1, "In review": 2, "Beta": 3, "Launched": 4 }
   # after_create_commit { broadcast_append_to "wishes" }
+
+  # COLORS = ["ECEDFE", "EFFEEC", "FEFCEC", "FEECEC", "F9ECFE", "ECFEFE"]
+
+  def stage_color
+    case stage
+      when "Backlog" then "#F9ECFE"
+      when "In process" then "#ECFEFE"
+      when "In review" then "#ECEDFE"
+      when "Beta" then "#FEFCEC"
+      when "Launched" then "#EFFEEC"
+    end
+  end
 end
