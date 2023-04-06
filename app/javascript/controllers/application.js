2056d09676b0453d9ac7c8aa2cc3e7c5dcb9e059
Daniel Rodriguez <daniel.rod93@gmail.com>
2023-04-05 21:25:31 -0600

    Feat: added coy to clipboard gem

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,10 @@
 import { Application } from "@hotwired/stimulus"
+import Clipboard from 'stimulus-clipboard'
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-
+application.register('clipboard', Clipboard)
 export { application }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,5 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+
