93f8014402c4fb2fdc6e54b3bd38dde0f358d188
Daniel Rodriguez <daniel.rod93@gmail.com>
2023-04-10 19:36:51 -0600

    Feat: added dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :wishlists, dependent: :destroy
   has_many :votes, dependent: :destroy
-  has_many :wishes, through: :votes
+  has_many :wishes, dependent: :destroy
   has_many :comments, dependent: :destroy
   belongs_to :organization, optional: true
   has_one_attached :photo
