2d67100ca5cf10e91b19c2c0d786c8c67956dc10
Daniel Rodriguez <daniel.rod93@gmail.com>
2023-05-05 16:35:37 +0100

    Feat: added comments create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,29 @@ class CommentsController < ApplicationController
         @comment = Comment.new(comment_params)
         @comment.user = current_user
         @comment.wish = Wish.find(params[:wish_id])
+        @iwsh = @comment.wish
         if @comment.save
             redirect_to wish_path(@comment.wish)
         else
             render 'wishes/show', status: :unprocessable_entity
         end
+    end
+
+    def edit
+        @comment = Comment.find(params[:id])
+        @wish = Wish.find(params[:wish_id])
+    end
+
+    def update
+        @comment = Comment.find(params[:id])
+        @comment.update(comment_params)
+        redirect_to wish_path(@comment.wish)
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        redirect_to wish_path(@comment.wish)
     end
 
     private
