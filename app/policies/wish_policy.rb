e060914e01e656a97fe78841b0f92296efebf675
Daniel Rodriguez <daniel.rod93@gmail.com>
2023-05-14 20:12:38 +0100

    Fix: changed scope for wishes

--- app/policies/wish_policy.rb
+++ app/policies/wish_policy.rb
@@ -2,7 +2,7 @@ class WishPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.where(user: user)
+      scope.all
     end
   end
 
