b239ce09b8e8ba52148a9aab530f7ee9a7389773
Christina Sugiono <christina.sugiono95@gmail.com>
2023-05-31 09:12:56 +0800

    fix: filter by stage on index page and remove filtering

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -2,7 +2,6 @@ class WishesController < ApplicationController
     skip_before_action :authenticate_user!, only: [ :show, :index ] if :verify_private_wishlist
     after_action :verify_authorized
 
-
     def show
         @wish = Wish.find(params[:id])
         @wishlist = @wish.wishlist
@@ -81,7 +80,7 @@ class WishesController < ApplicationController
     private
 
     def stage_params
-        params.permit(:stage)
+        params.permit(stage: [])
     end
 
 
