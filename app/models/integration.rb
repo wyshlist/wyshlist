369d24d0f757b91bdafef09b5e257a5b1546da19
Daniel Rodriguez <daniel.rod93@gmail.com>
2023-05-31 17:45:07 +0100

    dixed Asana integration

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -6,12 +6,4 @@ class Integration < ApplicationRecord
   validates :api_token, presence: true
   APPS = %w[Asana].freeze
   ACTIONS = ["Create a new task"].freeze
-  # after_create :create_automation
-
-  # def create_automation
-  #   case name
-  #   when "asana" && action == "Create a new task"
-  #     AsanaApi::SendTask.new(self.api_token).call(self.)
-  #   end
-  # end
 end
--- app/services/asana_api/client.rb
+++ app/services/asana_api/client.rb
@@ -10,48 +10,4 @@ module AsanaApi
             end
         end
     end
-
-    class GetWorkshops < ApplicationService
-        def initialize(api_token)
-            super(api_token)
-            @client = AsanaApi::AsanaClient.new(@api_token).call
-        end
-
-        def call
-            @client.workspaces.find_all
-        end
-    end
-
-    class GetProjects < ApplicationService
-        def initialize(api_token)
-            super(api_token)
-            @client = AsanaApi::AsanaClient.new(@api_token).call
-        end
-
-        def call(workshop)
-            @client.projects.get_projects(workspace: workshop.gid)
-        end
-    end
-
-    class GetTasks < ApplicationService
-        def initialize(api_token)
-            super(api_token)
-            @client = AsanaApi::AsanaClient.new(@api_token).call
-        end
-
-        def call(project)
-            @client.tasks.find_by_project(project: project.gid)
-        end
-    end
-
-    class SendTask < ApplicationService
-        def initialize(api_token)
-            super(api_token)
-            @client = AsanaApi::AsanaClient.new(@api_token).call
-        end
-
-        def call(workspace, project, task, description)
-            @client.tasks.create_in_workspace(workspace: workspace, projects: [project], name: task, notes: description)
-        end
-    end
 end
