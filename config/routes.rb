8df5f591061fd79cdda408b69b7cd34dd1aafa10
Christina Sugiono <christina.sugiono95@gmail.com>
2023-07-25 11:36:52 +0800

    solve visit other organization's wishlist

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/constraints/subdomain_constraint'
+require_relative '../lib/constraints/wishlist_constraint'
 
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
@@ -21,20 +22,22 @@ Rails.application.routes.draw do
   end
 
   constraints SubdomainConstraint do
-
     resources :organizations, only: [:edit, :update, :destroy, :show]
 
-    resources :wishlists, except: :show do
-      resources :integrations, only: [:new, :create]
-      resources :wishes, only: [:new, :create, :index]
+    constraints WishlistConstraint do
+      resources :wishlists, except: :show do
+        resources :integrations, only: [:new, :create]
+        resources :wishes, only: [:new, :create, :index]
+      end
+
+      resources :wishes, only: [:show, :edit, :update, :destroy] do
+        resources :votes, only: [:create]
+        resources :comments, only: [:create, :destroy, :edit, :update]
+      end
+
+      resources :votes, only: :destroy
     end
 
-    resources :wishes, only: [:show, :edit, :update, :destroy] do
-      resources :votes, only: [:create]
-      resources :comments, only: [:create, :destroy, :edit, :update]
-    end
-
-    resources :votes, only: :destroy
     resources :integrations, only: :destroy
 
     resources :users, only: [:show] do
