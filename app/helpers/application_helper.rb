9bf96c455fb16f18c1d46f171f7b6985e870d2fb
Daniel Rodriguez <daniel.rod93@gmail.com>
2023-09-15 16:14:41 +0200

    Removed trailig white space

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,5 @@ module ApplicationHelper
     def end_user_views?
         disallowed_controllers = ["wishes", "organizations", "users/registrations", "devise/sessions", "pages"]
         !disallowed_controllers.include?(params[:controller])
-      end
+    end
 end
