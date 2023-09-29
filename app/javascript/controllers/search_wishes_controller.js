204bbbf686985fe6b448cc2ced6192eb2eaca0a6
Christina Sugiono <christina.sugiono95@gmail.com>
2023-09-29 19:39:13 +0800

    temp placeholder for filters

--- app/javascript/controllers/search_wishes_controller.js
+++ app/javascript/controllers/search_wishes_controller.js
@@ -4,12 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form", "input"]
 
-  connect() {
-    console.log("search wishes");
-  }
-
   search(event) {
-    console.log('open search bar');
     this.formTarget.classList.toggle('d-none')
     this.inputTarget.classList.toggle('active')
     event.currentTarget.classList.toggle('active')
