8b436f09e821213e0cbe2c5b51ee1a1fa23bf261
Daniel Rodriguez <daniel.rod93@gmail.com>
2023-10-06 10:58:57 +0200

    Cleaned unused code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-    def dashboard_views?
-        allowed_view_paths = ["/feedback", "/members", "/organizations", "wishes", "wishlists/new"]
-        allowed_view_paths.include?(request.path)
-    end
 end
