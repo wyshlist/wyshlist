5cd75c00dce82f19bae333f96774654a489cf2e5
Daniel Rodriguez <daniel.rod93@gmail.com>
2023-10-06 10:57:02 +0200

    Modified navbar logic and adapted to organization new and edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def dashboard_views?
-        allowed_view_paths = ["/feedbacks", "/members", "/organizations", "wishes", "wishlists/new"]
+        allowed_view_paths = ["/feedback", "/members", "/organizations", "wishes", "wishlists/new"]
         allowed_view_paths.include?(request.path)
     end
 end
