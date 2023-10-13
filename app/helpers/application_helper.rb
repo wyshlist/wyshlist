b53dc3adcf3dac7ac0fe51bfa4a49c8b9dd7d59f
Christina Sugiono <christina.sugiono95@gmail.com>
2023-10-13 14:55:13 +0800

    add helper methods to user/team/pages navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def team_views?
+    (params[:controller].match?(/organizations|invitations/) &&
+      !(params[:controller].match?('organizations') && params[:action].match?('new'))) ||
+      (params[:controller].match?('wishlists') && params[:action].match?('new')) ||
+      (params[:controller].match?('users/registrations') && params[:action].match?('edit') && current_user.role != 'user')
+  end
+
+  def pages_view?
+    (params[:controller].match?('organizations') && params[:action].match?('new')) ||
+      (params[:controller].match?(/pages|users\/sessions/) ||
+      (params[:controller].match?('users/registrations') && params[:action].match?('new')) ||
+      (params[:controller].match?('users/registrations') && params[:action].match?('edit') && current_user.role == 'user'))
+  end
+
+  def end_user_views?
+    team_views? == false && pages_view? == false
+  end
 end
