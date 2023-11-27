ed171f4191ca1070805f79f3bb99cc4062ab5973
Christina Sugiono <christina.sugiono95@gmail.com>
2023-11-27 16:46:59 +0800

    style: clean up margin

--- app/javascript/controllers/show_ticket_details_controller.js
+++ app/javascript/controllers/show_ticket_details_controller.js
@@ -2,17 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="show-ticket-details"
 export default class extends Controller {
-  static targets = ["ticket"]
+  // connect() {
+  //   this.activeTicket = null
+  // }
 
-  connect() {
-    this.activeTicket = null
-  }
-
-  active(event) {
-    if (this.activeTicket) {
-      this.activeTicket.classList.remove('active')
-    }
-    this.activeTicket = event.currentTarget
-    event.currentTarget.classList.add('active')
-  }
+  // active(event) {
+  //   if (this.activeTicket) {
+  //     this.activeTicket.classList.remove('active')
+  //   }
+  //   this.activeTicket = event.currentTarget
+  //   event.currentTarget.classList.add('active')
+  // }
 }
--- app/javascript/controllers/ticket_popup_controller.js
+++ app/javascript/controllers/ticket_popup_controller.js
@@ -3,6 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="ticket-popup"
 export default class extends Controller {
   static targets = ["popup"]
+  connect() {
+    this.activeTicket = null
+  }
+
+  active(event) {
+    if (this.activeTicket) {
+      this.activeTicket.classList.remove('active')
+    }
+    this.activeTicket = event.currentTarget
+    event.currentTarget.classList.add('active')
+  }
 
   show() {
     this.popupTarget.classList.remove('d-none')
